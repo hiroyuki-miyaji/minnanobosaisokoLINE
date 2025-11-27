1a1205759f727578383a80452a9fedcd2789fb2c
flat-it.corp <53145040+hiroyuki-miyaji@users.noreply.github.com>
2025-11-27 15:59:47 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://prod-49.japaneast.logic.azure.com:443/workflows/6506d34d5ae94bdcb61ca0c6bf5ff23d/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=om4cTi9hss7-usRhKWnT9n9AIuMN9qq88GGtvZhmmvw";
+const API_URL = "https://prod-56.japaneast.logic.azure.com:443/workflows/274fd28c939748a3aee0c6a1994e80b4/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=2Iqm_M4sYZSIkPMMYdUfRoimdrNfiEBYsKZbU50sImA";
 
 async function callApi(body) {
     const res = await fetch(API_URL, {
