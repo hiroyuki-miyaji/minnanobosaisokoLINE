8f94038b1314e13f6a8a7f9dd500d2486987c04d
flat-it.corp <53145040+hiroyuki-miyaji@users.noreply.github.com>
2025-11-27 19:27:41 +0900

    Update liff.js

--- js/liff.js
+++ js/liff.js
@@ -1,20 +1,4 @@
 // js/liff.js
-async function initLiff(liffId) {
-    await liff.init({ liffId });
-    if (!liff.isLoggedIn()) {
-        liff.login();
-        return;
-    }
-}
-
-async function getLineId() {
-    const profile = await liff.getProfile();
-    return profile.userId;
-}
-
-window.LIFF_ID = "2008561144-5WLWMVPP";
-
-// js/liff.js
 // LIFF 共通デバッグ機能
 
 function debug(msg) {
